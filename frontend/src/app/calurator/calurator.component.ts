2ca9ef33d854f3f9d67bfabe3bc603ed7860bca5
mok <matipongpa@gmail.com>
2024-11-22 20:25:34 +0700

    add pie chart

--- frontend/src/app/calurator/calurator.component.ts
+++ frontend/src/app/calurator/calurator.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';Chart.register(ArcElement, Tooltip, Legend);
 import {
   FormControl,
   FormGroup,
@@ -13,7 +14,9 @@ import {
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
-
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartDataset, ChartOptions,PieController } from 'chart.js';
+Chart.register(PieController,ArcElement, Tooltip, Legend);
 @Component({
   selector: 'app-calurator',
   standalone: true,
@@ -25,6 +28,7 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
     MatIconModule,
     NgxMaskDirective,
     ReactiveFormsModule,
+    BaseChartDirective,
   ],
   providers: [provideNgxMask()],
   templateUrl: './calurator.component.html',
@@ -34,18 +38,30 @@ export class CaluratorComponent implements OnInit {
   form!: FormGroup;
   isResult!: boolean;
 
+  expensePieChart: ChartDataset<'pie'> = {
+    data: [],
+    label: 'expense',
+    backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+  };
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    aspectRatio:1,
+  };
+
+  pieChartDataset: ChartDataset[] = [];
+  public pieChartLabels: string[] = [];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
   constructor(private fb: FormBuilder) {}
+
+  @ViewChild(BaseChartDirective) expenseChart: BaseChartDirective | undefined;
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      income: new FormControl(
-        { value: null, disabled: false },
-        Validators.required,
-      ),
-      result: new FormControl(
-        { value: null, disabled: false },
-        Validators.required,
-      ),
+      income: new FormControl(0, Validators.required),
+      result: new FormControl(0, Validators.required),
       expense: this.fb.array([]),
     });
 
@@ -55,10 +71,7 @@ export class CaluratorComponent implements OnInit {
           { value: null, disabled: false },
           Validators.required,
         ),
-        value: new FormControl(
-          { value: null, disabled: false },
-          Validators.required,
-        ),
+        value: new FormControl(0, Validators.required),
       }),
     );
   }
@@ -88,6 +101,9 @@ export class CaluratorComponent implements OnInit {
     if (!this.form.valid) {
       return;
     }
+    this.pieChartDataset = []
+    this.pieChartLabels = []
+    this.expensePieChart.data = []
     this.expenseArray.controls.forEach((expense) => {
       this.form
         .get('result')
@@ -95,11 +111,22 @@ export class CaluratorComponent implements OnInit {
           this.form.get('result')?.value -
             (expense as FormGroup).get('value')?.value,
         );
+      this.expensePieChart.data.push(
+        +(expense as FormGroup).get('value')?.value,
+      );
+      this.pieChartLabels.push((expense as FormGroup).get('expense')?.value);
     });
+    this.pieChartDataset.push(this.expensePieChart);
     this.isResult = true;
   }
 
   deleteLesson(index: number) {
+    const expense = +(this.expenseArray.at(index) as FormGroup).get('value')
+      ?.value;
+    if (!isNaN(expense)) {
+      const currentResult = this.form.get('result')?.value || 0;
+      this.form.get('result')?.setValue(currentResult + expense);
+    }
     this.expenseArray.removeAt(index);
   }
 }
